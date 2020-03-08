c62f08a9b295452f2c0b38eb34a21c7b92e6e71a
Cathy D'Onofrio <catd825@gmail.com>
2020-03-08 02:32:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-ruby display_board.rb
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
